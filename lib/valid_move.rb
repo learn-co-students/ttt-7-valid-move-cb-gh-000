63a087c26e7780fc388015198e6f2c3664058862
Andrzej Hardie-Douglas <andrzejhd@gmail.com>
2017-01-24 10:35:55 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
 
+def valid_move?(validation_array, validation_index)
+  if position_taken?(validation_array, validation_index)
+    return false
+  else if validation_index <= 8 && validation_index >=0
+    return true
+  end
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(array, index)
+  if array[index] == "  " || array[index] == " " || array[index] == "" || array[index] == nil
+    return false
+  else
+    return true
+  end
+end
