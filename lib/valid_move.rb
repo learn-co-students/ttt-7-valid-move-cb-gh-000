6d86dcf59b6e04c294b4df9f77368d69083fb597
Kabelo Moiloa <moiloakabelo@gmail.com>
2017-01-25 15:26:35 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  index_valid = index >= 0 && index < board.length
+  !board.empty? && index_valid  && !position_taken?(board, index)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  !board.empty? && board[index] != nil && board[index].strip != ""
+end
