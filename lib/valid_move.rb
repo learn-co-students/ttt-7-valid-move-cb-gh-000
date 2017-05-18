c0dff3a1c165a4fea7c8150e028f7b9fef8e6c23
Sam Ebertz <samuel.ebertz@gmail.com>
2017-05-18 18:45:07 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(b,i)
+  --i
+  return i.between?(0,8) && !position_taken?(b,i)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(b,i)
+  return !!b[i] && b[i].strip != ""
+end
