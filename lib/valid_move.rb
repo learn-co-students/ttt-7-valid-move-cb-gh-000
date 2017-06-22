224af802b18d8ae26450b7dfd553767a4c804c9f
Esphas Kueen <esphas@hotmail.com>
2017-06-22 00:52:40 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,9 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? board, index
+  !board[index].nil? && board[index].strip != ''
+end
+def valid_move? board, index
+  (0...board.size).include?(index) && !position_taken?(board, index)
+end
