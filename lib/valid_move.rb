a7a1a43ea3f8db79fb856fde493d61f07e1ab89f
Brennan Brown <brennan.brown@edu.sait.ca>
2017-07-28 20:25:59 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# Method to check if position on Tic-tac-toe board is valid.
+def valid_move?(board, index)
+    # First checks if position is filled.
+    if board[index] == "X" || board[index] == "O"
+        return false
+    # Second checks if position is on board.
+    elsif index > 8
+        return false
+    # Finally allows move if previous requirements were met.
+    elsif board[index] == " " || board[index] == "" || board[index] == nil
+        return true
+    end
+end
