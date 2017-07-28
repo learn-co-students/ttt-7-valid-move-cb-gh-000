612cc1179a6d5f745aebcc9d677c26cf833f0b23
Dao Minh <1312359@student.hcmus.edu.vn>
2017-07-28 05:04:07 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? board, index
-  if !board.nil? && index >= 0 && index < board.count
+  if !board.nil? && !index.nil? && index >= 0 && index < board.count
     if board[index] == " " || !board[index] || board[index] == ""
       false
     else
