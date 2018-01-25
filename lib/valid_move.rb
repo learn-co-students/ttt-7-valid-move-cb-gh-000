c1bfe4b9059c1ea1a98ce508c2b921e910354646
Arie Tando <13510018@std.stei.itb.ac.id>
2018-01-25 06:41:00 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,23 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if (!between(index))
+    false
+  elsif (!(position_taken?(board, index)))
+    true
+  else
+    false
+  end
+end
 
+def between(index)
+  (index >= 0 && index < 9) ? true : false
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index].strip == ""
+    false
+  else
+    true
+  end
+end
