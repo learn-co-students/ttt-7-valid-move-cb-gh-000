d6e34acf61bbdc94b763ee62437cc56487047acb
Harkirat Singh <harkirat.hira@gmail.com>
2018-03-23 20:24:33 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
-
+def valid_move?(fir, sec)
+  if (sec < 0) || (sec > 8)
+    return false
+  elsif position_taken?(fir,sec)
+    return false
+  else
+    return true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(fir, sec)
+  if (fir[sec] == " ") || (fir[sec] == "") || (fir[sec] == nil)
+    return false
+  else
+    return true
+  end
+end
