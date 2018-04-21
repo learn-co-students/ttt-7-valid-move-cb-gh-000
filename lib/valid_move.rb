8a642d0d33ac8cc052e570a794b62a1054a89eb3
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-04-21 09:49:30 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
-
+def valid_move?(b,i)
+  i.between?(0,8) && !position_taken?(b, i)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(b,i)
+  if b[i]==" " || b[i]=="" || b[i]==nil
+    return false
+  else
+    return true
+  end
+end
