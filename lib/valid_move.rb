8d348a0ed35081a526e8f728dc6d5ad729e0255a
Shane Glass <shaneryanglass88@gmail.com>
2018-08-08 22:32:27 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+ return !position_taken?(board, index) && index >= 0 && index <= 9
+end
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# code your #position_taken? method here!
+
+def position_taken?(board, index)
+  square = board[index]
+  if square == nil
+    FALSE
+  elsif square.strip == ""
+    FALSE
+  else
+    TRUE
+  end
+end
