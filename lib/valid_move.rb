e340d96151e1e5ad2032ea10e441d4bcef6d4e28
Santiago Gonzalez <sagodi10581@gmail.com>
2018-12-10 11:50:28 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
-# code your #valid_move? method here
+def valid_move?(b,i)
+    if position_taken?(b,i) == false && i <= 8
+      true
+    else
+      false
+    end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(b,i)
+  if b[i] == "" || b[i] == " " || b[i] == nil
+    false
+  elsif b[i] == "X" || b[i] == "O"
+    true
+  end
+end
