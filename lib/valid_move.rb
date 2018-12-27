d0d8d2dcb4da21cbaede15807cd5b6d27e57562b
YUNFEI HU <yunfeih1@student.unimelb.edu.au>
2018-12-27 08:31:09 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if index.to_i.between?(1, 9) && board[index.to_i - 1] == " "
+    return true
+  else
+    return false
+  end
+end
