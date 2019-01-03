8e1317f8e54d7cf41d83439ec0d6e3a93ee314dc
Anmol Jande <jandanmol@gmail.com>
2019-01-03 18:03:33 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
 
+def valid_move?(board,num)
+  if num>=0 && num<=8 && position_taken?(board,num)==FALSE
+    return TRUE
+  else
+    return FALSE
+  end
+end
 
+def position_taken?(board,index)
+  
+if(board[index]==" " || board[index]=="")
+  return FALSE #not taken
+elsif(board[index]=="X" || board[index]=="O")
+return TRUE#takes
+else
+  return FALSE #if array is nil 
+end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
