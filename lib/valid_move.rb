cde149eb451ed17bfca39842bf7aea7a8fe4f8b2
Damion Mounts <damionotg@gmail.com>
2019-05-15 01:35:58 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  if position < 0 || position > 8
+    return false
+  else
+    return !position_taken?(board, position)
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  return board[position] == "X" || board[position] == "O"
+end
