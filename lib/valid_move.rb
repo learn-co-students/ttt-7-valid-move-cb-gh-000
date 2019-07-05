47f1957fd33ca4f620c77b14a1a2c57a73011228
Atul Tiwari <atultiwarix7@gmail.com>
2019-07-05 13:11:47 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,15 @@
+def position_taken?(board,index)
+  if board[index]=="" || board[index]==" " || board[index]==nil
+    return false
+  else board[index]=="X" || board[index]=="O"
+    return true
+  end
+end
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  is=position_taken?(board,index)
+  if index.between?(0,8)==true && is==false
     return true
   else
     return false
   end
-
-
 end
