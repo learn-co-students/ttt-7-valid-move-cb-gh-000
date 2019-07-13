f63c5fca8fdbd2667046ed2a92975babf2044ffe
cyprium dev <cyprium@protonmail.com>
2019-07-13 00:35:17 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if index >= 0 && index <= 8
+    if position_taken?(board, index) == true
+      return false
+    elsif position_taken?(board, index) == false
+      return true
+    else
+      return true
+    end
+  else
+    return false
+  end
 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
