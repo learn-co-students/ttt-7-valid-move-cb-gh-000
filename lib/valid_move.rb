0fe69692e8e433da0966904505b8c51828844bc4
Austin Wentworth <austin.wentworth@hotmail.com>
2019-08-02 23:45:41 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,5 +4,5 @@ def valid_move?(board, index)
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  return ["X", "O"].include?(board[index])
+  return board[index] != " "
 end
