d27a76418eac11344b6b0abc1742c3c96c0d2bcd
Ri Xin Yang <rixin.yang.2001@gmail.com>
2019-12-16 23:16:47 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,19 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+
+def valid_move?(board, index)
+  if index >= 0 && index <= 9 && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
