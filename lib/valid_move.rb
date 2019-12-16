6a9593f79fb8b9809b18b3be9f0773ef433d9e1b
Ri Xin Yang <rixin.yang.2001@gmail.com>
2019-12-16 23:17:08 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index >= 0 && index <= 9 && !position_taken?(board, index)
+  if index >= 0 && index <= 8 && !position_taken?(board, index)
     true
   else
     false
